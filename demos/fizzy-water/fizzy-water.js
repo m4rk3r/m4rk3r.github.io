5dc62cc3359398fc94a57e5477a91db6f8d71a6f
Mark Beasley <mbeasl1@gmail.com>
2020-03-04 19:29:46 -0500

    Move demos to dedicated folder

---
+++ demos/fizzy-water/fizzy-water.js
@@ -0,0 +1,11 @@
+
+function popBubble(event) {
+  let hoverObject = event.target;
+  if (hoverObject.classList.contains('bubble')) {
+    hoverObject.classList.add('pop');
+  }
+}
+
+
+let bubbleWrapper = document.querySelector('.bubble-wrapper');
+bubbleWrapper.addEventListener('mouseover', popBubble);
